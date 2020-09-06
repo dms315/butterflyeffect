7e7522bf2f76062391747234afab717cdecf3f8b
dms315 <eunjeong89@gmail.com>
2020-09-06 13:53:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+$(document).ready(function(){
+
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
+    width: 400,
     loop: true,
   
-  
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
 })
+});
